df6a0be9b32a6c3e6da0fe36eb06a1aa1a6801df
ahsanazeemi <64397746+ahsanazeemi@users.noreply.github.com>
2020-07-07 16:33:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+// Stop Watch Assignment //
+
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+var inter;
+var mintxt = document.getElementById('min');
+var sectxt = document.getElementById('sec');
+var msectxt = document.getElementById('msec');
+
+function stopwatch(){
+    msec++;
+    msectxt.innerHTML = msec;
+    if(msec <10){
+        msectxt.innerHTML = '0'+msec;
+    }
+    if(msec >=100){
+        sec++;
+        msec =0;
+        sectxt.innerHTML = sec;
+    }else if (sec >= 60){
+        sec=0;
+        min++;
+        mintxt.innerHTML = min;
+    }
+    if(sec <10){
+        sectxt.innerHTML = '0'+sec;
+    }
+    if(min <10){
+        mintxt.innerHTML = '0'+min;
+    }
+}
+
+function start(){
+    inter = setInterval(stopwatch, 10)
+    document.getElementById('but').hasAttribute.disabled;
+}
+function stop(){
+    clearInterval(inter)
+}
+function reset(){
+    min = 0;
+    sec = 0;
+    msec = 0;
+    mintxt.innerHTML = '0'+min;
+    sectxt.innerHTML = '0'+sec;
+    msectxt.innerHTML = '0'+msec;
+    stop()
+}
+
